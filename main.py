14e27bd9319731fe206f42430014c9069206b3a8
realWalter05 <78427254+realWalter05@users.noreply.github.com>
2021-05-14 15:40:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def get_new_msgs():
             "login": "",
         }
         page_komens = send_payload("https://zsebenese.bakalari.cz/Login",
-                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=rok",
+                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=mesic",
                                    payload)
 
         idmsgs = get_new_idmsgs(page_komens, old_idmsgs)
@@ -180,7 +180,7 @@ def get_new_msgs():
             "login": "",
         }
         page_komens = send_payload("https://zsebenese.bakalari.cz/Login",
-                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=rok",
+                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=mesic",
                                    payload)
 
         msgs = get_msgs(get_idmsg(page_komens))
