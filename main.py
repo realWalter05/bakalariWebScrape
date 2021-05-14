884e9e846053b2aa7bef9540f6d64e9f4d4c34f8
realWalter05 <78427254+realWalter05@users.noreply.github.com>
2021-05-14 14:48:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def get_new_msgs():
             "login": "",
         }
         page_komens = send_payload("https://zsebenese.bakalari.cz/Login",
-                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=rok",
+                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=2polo",
                                    payload)
 
         msgs = get_msgs(get_idmsg(page_komens))
