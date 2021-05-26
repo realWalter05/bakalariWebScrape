345e59484f5546914bc618b868426ae4452dd4ed
realWalter05 <78427254+realWalter05@users.noreply.github.com>
2021-05-26 19:12:19 +0200

    Update main.py
    Removed seperetad updates

--- main.py
+++ main.py
@@ -163,11 +163,6 @@ def get_new_msgs():
             return render_template('index.html', msgs=old_idmsgs, status=":ú")
     # We're here from index to setup first msgs
     else:
-        number = request.args.get("number")
-        start_number = 30
-        if number:
-            start_number = int(number)
-
         print("There are no msgs yet")
         payload = {
             "username": "zikav29z",
@@ -176,24 +171,11 @@ def get_new_msgs():
             "login": "",
         }
 
-        # Getting msgs separately because request error h12
-        half_year_back = date.today() + relativedelta(days=-start_number)
-        start_month = str(half_year_back.day) + str(half_year_back.month) + str(half_year_back.year)
-
-        half_year_back_end = half_year_back + relativedelta(days=+5)
-        if start_number == 5:
-            half_year_back_end = date.today()
-
-        end_month = str(half_year_back_end.day) + str(half_year_back_end.month) + str(half_year_back_end.year)
-
         page_komens = send_payload("https://zsebenese.bakalari.cz/Login",
-                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=custom&l=prijate&from="+start_month+"&to="+end_month,
+                                   "https://zsebenese.bakalari.cz/next/komens.aspx?s=mesic",
                                    payload)
 
         msgs = get_msgs(get_idmsg(page_komens))
         msgs = group_msgs(sorted(msgs, key=lambda k: k['Jmeno']))
 
-        if start_number == 5:
-            return render_template('index.html', msgs=msgs, done="done")
-        print("leaving")
-        return render_template('index.html', msgs=msgs, number=(start_number - 5))
+        return render_template('index.html', msgs=msgs)
